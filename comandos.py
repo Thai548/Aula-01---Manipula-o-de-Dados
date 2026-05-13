a98aef6096ffdd68e49da754b656d9030b3de168
Thai548 <thaivicent@gmail.com>
2026-05-12 21:23:28 -0300

    Aula 01 - Modulo 2

---
+++ comandos.py
@@ -0,0 +1,8 @@
+
+# python -m venv venv
+# source ./venv/Scripts/activate  (para ativar o venv)
+# pip install pandas    (Para instalar bibliotecas)
+# deactivate (para desativar o venv)
+#  rm -rf venv (para apagar tudo da pasta)
+
+# pip install -r requirements.txt ( reinstalar utilizando o requirements)
